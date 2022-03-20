28f8044b1fbc1810e69d2a0463fbb2d54c161d70
Bachalo <michal.bastrzyk.pl@gmail.com>
2022-03-20 10:53:09 +0100

    IconWrapper: Added

---
+++ src/lib/IconWrapper.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let width = '16px';
+	export let height = '16px';
+	export let color = '#000';
+</script>
+
+<div class="icon" style={`width: ${width}; height: ${height}; color: ${color}`}>
+	<slot />
+</div>
